2ef1b27ff27d4b30a3490e4aac76be5b5e2bde33
Gehrig Barnes <gehrigbarnes@gmail.com>
2022-01-27 08:59:51 -0600

    added search bar

--- src/Components/ChessPage.js
+++ src/Components/ChessPage.js
@@ -1,12 +1,29 @@
 import '../css/ChessPage.css'
-import React from 'react';
+import React, {useState} from 'react';
 import OpeningsContainer from './OpeningsContainer'
+import Search from './Search'
 
 
 function ChessPage({ openings, handleRemoveCard }) {
+  const [search, setSearch] = useState('')
+  
+  
+  function handleSearch(e){
+    const value = e.target.value;
+    setSearch(value)
+  }
+
+function filterSearch(){
+  const filterSearch = openings.filter((opening) => {
+    return opening.name.toLowerCase().includes(search);
+  })
+  return filterSearch;
+}
+
   return (
     <div>
-      <OpeningsContainer openings={openings} handleRemoveCard={handleRemoveCard} />
+      <Search handleSearch={handleSearch}/>
+      <OpeningsContainer openings={filterSearch()} handleRemoveCard={handleRemoveCard} />
     </div>
   );
 }
